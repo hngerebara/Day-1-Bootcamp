ceb66768c83dbb718cd8050671604fd6a5837b8f
Hopaez Ngerebara <hope.ngerebara@gmail.com>
2017-03-16 14:47:20 +0100

    Car class updated

--- app/car.js
+++ app/car.js
@@ -4,57 +4,53 @@ function Vehicle(vehicletype){
   }
 }
 //inheritaning from super class vehicle
-function Car(vehicletype,carname,carmodel,cartype){ //
+function Car(vehicletype,name,model,type){ //
 Vehicle.call(this, vehicletype)
-if(typeof(vehicletype) === Car){
-  if(typeof(carname) !== 'string'){
-    this.carname = "General";
-  }
-  else{
-    this.carname = carname;
-  }
-  if(typeof(carmodel) !== 'string'){
-    this.carmodel = "GM";
-  }
-  else{
-    this.carmodel = carmodel;
-  }
-  
-  this.carspeed = "0 km/h";
-    this.drive = function(val){
-      this.gear = val;
-      if(this.carname === "Porshe"){
-        this.carspeed = "250 km/h";
+    if(typeof(vehicletype) == typeof("")){
+      this.vehicletype = vehicletype;
+    }
+    else{
+      this.vehicletype = "Car";
+    }
+  if(typeof(name) == typeof("")){
+      this.name = name;
+    }
+    else{
+      this.name = "General";
+    }
+    if(typeof(model) == typeof("")){
+      this.model = model;
+    }
+    else{
+      this.model = "GM";
+    }
+      this.type = type;
+      if(this.name === "Porshe" || this.name === "Koenigsegg"){
+        this.numOfDoors = 2;
       }
-        
-      if(this.carname === "MAN"){
-        this.carspeed ="77 km/h";
+      else{
+        this.numOfDoors = 4;
       }
+      this.speed = "0 km/h";
+      this.drive = function(number){
+        this.gear = number;
+        if(this.name === "Porshe")
+          this.speed = "250 km/h";
+        if(this.name === "MAN")
+          this.speed ="77 km/h";
         return this; 
-    }
-      
-  this.cartype = cartype;
-    if(this.carname === "Porshe" || this.carname === "Koenigsegg"){
-      this.numOfDoors = 2;
-    }
-    else{
-      this.numOfDoors = 4;
-    }
-
-    if(this.cartype === "trailer"){
-      this.numOfWheels = 8;
-      this.isSaloon = false;
-    }
-    else{
-      this.numOfWheels = 4;
-      this.isSaloon = true;
       }
-
-}    
-}
-
-
-  module.exports={
-  Vehicle:Vehicle,
-  Car:Car 
-  }
+      if(this.type === "trailer"){
+        this.numOfWheels = 8;
+        this.isSaloon = false;
+      }
+      else{
+        this.numOfWheels = 4;
+        this.isSaloon = true;
+      }
+      
+    }
+    module.exports={
+      Vehicle:Vehicle,
+      Car:Car
+    }
